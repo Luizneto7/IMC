b74c030a55ab489af1eaa0cc62b241acf2c55cfc
luizneto7 <luiz.bnp2@gmail.com>
2024-01-10 08:29:12 -0300

    Function clear Input addiction

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,35 @@
+import { inputHeight, inputWeight } from "./script.js"
+
 export const Modal = {
+    message: document.querySelector(".title span"),
     wrapper: document.querySelector(".modal-wrapper"),
-    message: document.querySelector(".modal .title span"),
     buttonClose: document.querySelector(".close"),
 
-    open () {
+    open() {
         Modal.wrapper.classList.add("open")
     },
 
-    close () {
+    close() {
         Modal.wrapper.classList.remove("open")
-    }
+    },
+
+    clearInput () {
+        inputHeight.value = ""
+        inputWeight.value = ""
+    },
 }
 
 Modal.buttonClose.onclick = () => {
     Modal.close()
+    Modal.clearInput()
 }
+
+window.addEventListener("keydown", handleKeyDown)
+
+function handleKeyDown (event){
+    if(event.key === "Escape"){
+        Modal.close()
+        Modal.clearInput()
+    }
+}
+
--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-import {Modal} from "./modal.js"
+import { Modal } from './modal.js'
 
+export const inputWeight = document.querySelector("#weight")
+export const inputHeight = document.querySelector("#height")
 const form = document.querySelector("form")
-const inputWeight = document.querySelector("#weight")
-const inputHeight = document.querySelector("#height")
 
 form.onsubmit = event => {
     event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
-
     const result = imc(weight, height)
-    const message = `Seu IMC é de ${result}`
-    
+    const message = `Seu IMC é igual a ${result}`
+
     Modal.message.innerText = message
+
     Modal.open()
 }
 
-function imc(weight, height){
-    return ((weight / (height /100) ** 2)).toFixed(2)
+function imc(weight, height) {
+    return ((weight / (height / 100) ** 2)).toFixed(2)
 }
